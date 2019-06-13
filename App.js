91ae0e1af2fb362b8d9917ee4c3a5f391a85253d
PauloRev <paulohenriquerev@gmail.com>
2019-06-12 22:33:54 -0300

    Adicionado estilo ao cronometro

--- App.js
+++ App.js
@@ -1,49 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Image source={require('./images/cronometro.png')} />
+        <Text style={styles.timer}>0.0</Text>
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.botao}>
+            <Text style={styles.btnTexto}>Vai</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botao}>
+            <Text style={styles.btnTexto}>Zerar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    );
+    )
   }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 30,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#00aeef'
   },
-  welcome: {
+  timer: {
+    marginTop: -170,
+    color: '#fff',
+    fontSize: 72,
+    fontWeight: 'bold'
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40
+  },
+  botao: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    height: 40,
+    margin: 20,
+    borderRadius: 10
+  },
+  btnTexto: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    fontWeight: 'bold',
+    color: '#00aeef'
+  }
+})
