e056f4cedd1e53e7485b26dd0d4a2efaa672c7b3
PauloRev <paulohenriquerev@gmail.com>
2019-06-12 22:50:23 -0300

    Adicionada logica para o cronometro

--- App.js
+++ App.js
@@ -2,17 +2,65 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      numero: 0,
+      botao: 'Vai'
+    }
+    // Variavel do timer do relogio
+    this.timer = null
+
+    this.vai = this.vai.bind(this)
+    this.zerar = this.zerar.bind(this)
+  }
+
+  vai() {
+    let state = this.state
+
+    if(this.timer != null) {
+      // Parar o timer
+      clearInterval(this.timer)
+      this.timer = null
+      state.botao = 'Vai'
+    } else {
+      // Começa o timer
+      this.timer = setInterval(() => {
+        let state = this.state
+        state.numero += 0.1
+        this.setState(state)
+      }, 100)
+      state.botao = 'Parar'
+    }
+    this.setState(state)
+  }
+
+  zerar() {
+    let state = this.state
+
+    if(this.timer != null) {
+      // Parar timer
+      clearInterval(this.timer)
+      this.timer = null
+    }
+
+    state.numero = 0
+    state.botao = 'Vai'
+    this.setState(state)
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Image source={require('./images/cronometro.png')} />
-        <Text style={styles.timer}>0.0</Text>
+        <Text style={styles.timer}>{this.state.numero.toFixed(1)}</Text>
         <View style={styles.btnArea}>
-          <TouchableOpacity style={styles.botao}>
-            <Text style={styles.btnTexto}>Vai</Text>
+          <TouchableOpacity style={styles.botao} onPress={this.vai}>
+            <Text style={styles.btnTexto}>{this.state.botao}</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.botao}>
-            <Text style={styles.btnTexto}>Zerar</Text>
+            <Text style={styles.btnTexto} onPress={this.zerar}>Zerar</Text>
           </TouchableOpacity>
         </View>
       </View>
